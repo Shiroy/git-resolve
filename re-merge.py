81e60f8a548962b7eb27eed88612108d95739db3
Matt <mkreileder@bloomberg.net>
2017-11-12 21:46:22 +0000

    Simple script to resolve a merge conflict

--- re-merge.py
+++ re-merge.py
@@ -5,19 +5,45 @@ repo = pygit2.Repository(os.getcwd())
 ind = repo.index
 ind.read()
 
+print("Index contains: " + str(len(ind)) + " elements")
+
 confs = ind.conflicts
 
 ind2 = pygit2.Index()
+paths = []
+ids = []
 for c in confs:
-	ind2.add(c[1]) 
+  ind2.add(c[1])
+  paths.append(c[1].path)
+  ids.append(c[1].id)
+	
 ind.clear()
 for e in ind2:
-	ind.add(e)
+  ind.add(e)
+
+for c in confs:
+  ind.add(c[1])
+
+print("Index contains: " + str(len(ind)) + " elements")
+
+ind.write()
 
 commit = repo.revparse_single('HEAD')
+merge_commit = repo.revparse_single('MERGE_HEAD')
 aut = pygit2.Signature('name here','email here')
 t = ind.write_tree()
-repo.create_commit('HEAD', aut, aut, 'some comment', t, [commit.id])
-with open(ind[0].path, 'wb') as fd:
-	fd.write(repo[ind[0].id].data)
+repo.create_commit('HEAD', aut, aut, 'some comment', t, [commit.id, merge_commit.id])
+
+for i in range(len(paths)):
+  with open(paths[i], 'wb') as fd:
+    fd.write(repo[ids[i]].data)
+
+
+os.remove(repo.path + '/MERGE_HEAD')
+os.remove(repo.path + 'MERGE_MODE')
+os.remove(repo.path + 'MERGE_MSG')
+
+
+
+
 
